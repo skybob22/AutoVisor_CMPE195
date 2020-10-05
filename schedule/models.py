76d7ce89b3dbdf222d3eb0ae13d070da7dc8b6b5
Steven West <stevewes97@gmail.com>
2020-10-04 20:41:02 -0700

    Added datatypes, Added some test entries to database

--- schedule/models.py
+++ schedule/models.py
@@ -16,14 +16,37 @@ class Post(models.Model):
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
 
-class Course(models.Model):
-	department = models.TextField()
-	courseID = models.TextField()
-	id = models.AutoField(primary_key=True)
+class Department(models.Model):
+	abrv = models.TextField(primary_key=True)
+	name = models.TextField()
 
-	class Meta: 
+	def __str__(self):
+		return self.abrv
+
+class Course(models.Model):
+	id = models.AutoField(primary_key=True)
+	department = models.ForeignKey(Department,on_delete=models.RESTRICT)
+	courseID = models.TextField()
+	prereqs = models.ManyToManyField('self',symmetrical=False,blank=True,related_name='Prerequisites')
+	coreqs = models.ManyToManyField('self',symmetrical=False,blank=True,related_name='Corequisites')
+
+	class Meta:
 		unique_together = ('department', 'courseID', )
 
 	def __str__(self):
-		return self.department + ' ' + self.courseID	
+		return str(self.department) + ' ' + self.courseID
 
+class Catalogue(models.Model):
+	id = models.AutoField(primary_key=True)
+	department = models.ForeignKey(Department,on_delete=models.RESTRICT)
+	year = models.IntegerField()
+	courses = models.ManyToManyField(Course,symmetrical=False)
+
+	class Meta:
+		unique_together = ('department','year',)
+
+	def __str__(self):
+		retStr = str(department) + ' ' + str(year) + ':'
+		for course in self.courses.all():
+			retStr = retStr + str(course) + ','
+		return retStr;
