520f4591d90aab4ac5e4d50522df63e48964192a
skybob22 <skybob22@users.noreply.github.com>
2020-10-04 18:03:16 -0700

    Initial Commit

---
+++ schedule/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+
+# from django.http import HttpResponse
+
+
+
+
+posts = [
+	{
+		'author': 'Daniel',
+		'title': 'First Post',
+		'content': 'First Post content',
+		'date_posted': 'July 27, 2020'
+	},
+	{
+		'author': 'Sarah',
+		'title': 'Second Post',
+		'content': 'Second Post content',
+		'date_posted': 'July 28, 2020'
+	}	
+
+]
+
+# Create your views here.
+def home(request):
+	context = {
+		'posts': Post.objects.all()
+	}
+	return render(request, 'schedule/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'schedule/home.html' # <app>/<model>_<viewtype>.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+	paginate_by = 5
+	
+class UserPostListView(ListView):
+	model = Post
+	template_name = 'schedule/user_posts.html' # <app>/<model>_<viewtype>.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+	paginate_by = 5
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date_posted')
+
+class PostDetailView(DetailView):
+	model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+	success_url = '/'
+	
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+
+def about(request):
+	return render(request, 'schedule/about.html', {'title': 'About'})
+
+
+@login_required
+def transcript(request):
+	return render(request, 'schedule/transcript.html', {'title': 'Transcript'})
+
+@login_required
+def roadmap(request):
+	return render(request, 'schedule/roadmap.html', {'title': 'Roadmap'})
+
+@login_required
+def community(request):
+	return render(request, 'schedule/community.html', {'title': 'Roadmap'})
