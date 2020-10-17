b096477a33335b3cce1f6678b5ac3e5aaf93f4dc
danielpark1 <photoncommand8415@gmail.com>
2020-10-16 22:21:31 -0700

    added database entries

--- users/forms.py
+++ users/forms.py
@@ -2,10 +2,12 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+#from django.schedule.models import Student
 
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
+	#student = models.OneToOneField('Student',on_delete=models.SET_NULL,default=None,blank=True,null=True)
 
 	class Meta:
 		model = User
