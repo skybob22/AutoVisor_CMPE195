f4c811028ae38b9fe980be9897070dd4e4f3b60c
DanielPark1 <Photoncommand8415@gmail.com>
2020-11-15 21:13:14 -0800

    Added deletion of single classes

--- schedule/forms.py
+++ schedule/forms.py
@@ -1,8 +1,23 @@
 from django import forms
-from .models import Department, Course, TranscriptGrade
+from .models import *
 
 class Select_Department_CMPE_Form(forms.ModelForm):
 
     class Meta:
         model = TranscriptGrade
         fields = ['course', 'grade']
+
+class TranscriptGradeDeleteForm(forms.Form):
+
+    class Meta:
+        model = TranscriptGrade
+        fields = ['course']
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(TranscriptGradeDeleteForm, self).__init__(*args, **kwargs)
+        self.fields['course'].queryset = TranscriptGrade.objects.filter(transcript=self.user.student.transcript)
+
+    course = forms.ModelChoiceField(queryset=None)
+
+class UserPreferenceForm(forms.ModelForm):
+    course = forms.ModelMultipleChoiceField(queryset=Course.objects.all())
